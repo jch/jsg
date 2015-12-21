5d6d834694b53d612d512720e5bdb6e29225c493
Jerry Cheung <jollyjerry@gmail.com>
2015-12-20 21:15:26 -0800

    avoid run helper entirely
    just use plain bash

--- test/bin/jsg_markdown_test.sh
+++ test/bin/jsg_markdown_test.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env test/bats/bin/bats
 
 @test "markdown" {
-  run bash -c 'echo "#header" | bin/jsg_markdown'
-  [ "$status" -eq 0 ]
-  echo "$output" | grep "<h1>"
+  echo "#header" | bin/jsg_markdown | grep "<h1>"
+  [ $? -eq 0 ]
 }
