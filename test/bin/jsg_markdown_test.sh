3bd9e42fc1cf7da3ad3b9a8a9cef665ac926748f
Jerry Cheung <jollyjerry@gmail.com>
2025-06-25 09:00:05 -0700

    update test gfm adds classes

--- test/bin/jsg_markdown_test.sh
+++ test/bin/jsg_markdown_test.sh
@@ -2,7 +2,7 @@
 
 @test "markdown" {
   output=$(cat 'test/fixtures/file.md' | bin/jsg_markdown)
-  echo $output | grep '<h1>'
+  echo $output | grep '</h1>'
   echo $output | grep '<p>paragraph one</p>'
   echo $output | grep '<p>paragraph two</p>'
 }
